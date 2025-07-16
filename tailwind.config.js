1e7b7edc91dca85273adae4573d15ff96edbfad2
gelii aceee <123482578+leykaaaaa@users.noreply.github.com>
2025-07-16 09:02:29 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            boxShadow: {
+                custom: "0 0 6px rgb(0, 0, 0, .3);",
+                custom2: "0 0 8px 1px rgb(250, 250, 250, .2);",
+            },
+        },
+    },
+    plugins: [],
+};
